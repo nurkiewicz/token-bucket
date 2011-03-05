ea818bc121ba10b46a2b0e72e766639f3ea2aa56
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-03-05 22:00:32 +0100

    JMX monitoring

--- src/main/java/com/blogspot/nurkiewicz/download/tokenbucket/GlobalTokenBucket.java
+++ src/main/java/com/blogspot/nurkiewicz/download/tokenbucket/GlobalTokenBucket.java
@@ -3,6 +3,8 @@ package com.blogspot.nurkiewicz.download.tokenbucket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,12 +15,15 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
+import static org.apache.commons.lang.Validate.isTrue;
+
 /**
  * @author Tomasz Nurkiewicz
  * @since 03.03.11, 00:02
  */
 @Service
 @Primary
+@ManagedResource
 public class GlobalTokenBucket extends TokenBucketSupport {
 
 	private static final Logger log = LoggerFactory.getLogger(GlobalTokenBucket.class);
@@ -26,7 +31,7 @@ public class GlobalTokenBucket extends TokenBucketSupport {
 	private Semaphore count = new Semaphore(0, false);
 	private ScheduledExecutorService executorService;
 
-	private int maxTokens = 10000;
+	private volatile int maxTokens = 10000;
 
 	@PostConstruct
 	public void startBucketFillingThread() {
@@ -53,11 +58,24 @@ public class GlobalTokenBucket extends TokenBucketSupport {
 	@Override
 	public boolean tryTake(HttpServletRequest req, int howMany) {
 		return count.tryAcquire(howMany);
-
 	}
 
 	@Override
 	public void completed(HttpServletRequest req) {
 	}
 
+	@ManagedAttribute
+	public int getMaxTokens() {
+		return maxTokens;
+	}
+
+	public void setMaxTokens(int maxTokens) {
+		isTrue(maxTokens >= 0);
+		this.maxTokens = maxTokens;
+	}
+
+	@ManagedAttribute
+	public int getOngoingRequests() {
+		return count.getQueueLength();
+	}
 }
