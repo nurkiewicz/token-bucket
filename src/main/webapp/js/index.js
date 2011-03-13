ecd6e95369cf2670be3500a97086d41c2d162a59
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-03-13 19:49:16 +0100

    Simple graph using jqPlot, WARNING: causes huge memory leak in all major browsers

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
 });
 
 function Monitor() {
+	var values = [];
 	var jmx = new Jolokia("/jolokia");
 
 	var loadMemoryUsage = function() {
 		var value = jmx.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", "used");
-
+		values.push(parseInt(value));
+		if (values.length > 1) {
+			$('#memoryChart')
+					.jqplot([values], { })
+					.replot();
+		}
 	};
 
 	this.schedule = function () {
