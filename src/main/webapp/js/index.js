0709a313762062cb4788b1111fd87c065c4743d3
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-03-14 22:30:18 +0100

    More generic initialization

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-	new Monitor();
+	new JmxChart('memoryChart', 'java.lang:type=Memory', 'HeapMemoryUsage', 'used');
 });
 
-function Monitor() {
+function JmxChart(id, mbean, attribute, path) {
 	var jmx = new Jolokia("/jolokia");
-
 
 	var chart = new Highcharts.Chart({
 		chart: {
-			renderTo: 'memoryChart',
+			renderTo: id,
 			animation: {
 				duration: 900,
 				easing: 'linear'
@@ -19,12 +18,11 @@ function Monitor() {
 				load: function() {
 					var series = this.series[0];
 					setInterval(function() {
-						var x = (new Date()).getTime();
-						var memoryUsed = jmx.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", "used");
+						var value = parseInt(jmx.getAttribute(mbean, attribute, path));
 						series.addPoint({
 							x: new Date().getTime(),
-							y: parseInt(memoryUsed)
-						}, true, series.data.length >= 50);
+							y: value
+						}, true, series.data.length >= 30);
 					}, 1000);
 				}
 			}
