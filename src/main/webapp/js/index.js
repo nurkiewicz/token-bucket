2447ed97873f1f850cc0639afe6839169267d89e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-03-14 23:27:39 +0100

    Several charts on a single page

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,42 +1,38 @@
 $(document).ready(function() {
 	new JmxChart('memoryChart', 'java.lang:type=Memory', 'HeapMemoryUsage', 'used');
+	new JmxChart('totalThreadsCountChart', 'java.lang:type=Threading', 'ThreadCount', '');
+	new JmxChart('httpBusyChart', 'Catalina:name="http-bio-8080",type=ThreadPool', 'currentThreadsBusy', '');
+	new JmxChart('httpQueueSize', 'Catalina:name=executor,type=Executor', 'queueSize', '');
 });
 
 function JmxChart(id, mbean, attribute, path) {
 	var jmx = new Jolokia("/jolokia");
 
-	var chart = new Highcharts.Chart({
+	new Highcharts.Chart({
 		chart: {
 			renderTo: id,
-			animation: {
-				duration: 900,
-				easing: 'linear'
-			},
+			animation: false,
 			defaultSeriesType: 'spline',
 			marginRight: 10,
 			events: {
 				load: function() {
 					var series = this.series[0];
 					setInterval(function() {
-						var value = parseInt(jmx.getAttribute(mbean, attribute, path));
-						series.addPoint({
-							x: new Date().getTime(),
-							y: value
-						}, true, series.data.length >= 30);
+						series.addPoint(getAttribute(), true, series.data.length >= 30);
 					}, 1000);
 				}
 			}
 		},
 
 		title: {
-			text: 'Live random data'
+			text: mbean
 		},
 		xAxis: {
 			type: 'datetime'
 		},
 		yAxis: {
 			title: {
-				text: 'HeapMemoryUsage'
+				text: attribute
 			}
 		},
 		legend: {
@@ -58,11 +54,18 @@ function JmxChart(id, mbean, attribute, path) {
 
 			{
 				type: 'spline',
-				data: [],
-				name: 'Rainfall'
+				data: [getAttribute()],
+				name: path || attribute
 			}
 		]
 	});
+
+	function getAttribute() {
+		return {
+			x: new Date().getTime(),
+			y: parseInt(jmx.getAttribute(mbean, attribute, path))
+		}
+	}
 
 
 }
