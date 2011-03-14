449020a7373ae5fd0d2221a2a01921e6502b3a57
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-03-14 22:22:33 +0100

    Basic memory chart based on Highcharts

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,19 +1,70 @@
 $(document).ready(function() {
-	new Monitor().schedule();
+	new Monitor();
 });
 
 function Monitor() {
 	var jmx = new Jolokia("/jolokia");
 
-	var loadMemoryUsage = function() {
-		var value = jmx.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", "used");
 
-	};
+	var chart = new Highcharts.Chart({
+		chart: {
+			renderTo: 'memoryChart',
+			animation: {
+				duration: 900,
+				easing: 'linear'
+			},
+			defaultSeriesType: 'spline',
+			marginRight: 10,
+			events: {
+				load: function() {
+					var series = this.series[0];
+					setInterval(function() {
+						var x = (new Date()).getTime();
+						var memoryUsed = jmx.getAttribute("java.lang:type=Memory", "HeapMemoryUsage", "used");
+						series.addPoint({
+							x: new Date().getTime(),
+							y: parseInt(memoryUsed)
+						}, true, series.data.length >= 50);
+					}, 1000);
+				}
+			}
+		},
 
-	this.schedule = function () {
-		setInterval(function() {
-			loadMemoryUsage()
-		}, 1000);
-	};
+		title: {
+			text: 'Live random data'
+		},
+		xAxis: {
+			type: 'datetime'
+		},
+		yAxis: {
+			title: {
+				text: 'HeapMemoryUsage'
+			}
+		},
+		legend: {
+			enabled: false
+		},
+		exporting: {
+			enabled: false
+		},
+		plotOptions: {
+			spline: {
+				lineWidth: 1,
+				marker: {
+					enabled: false
+				}
+			}
+		},
+
+		series: [
+
+			{
+				type: 'spline',
+				data: [],
+				name: 'Rainfall'
+			}
+		]
+	});
+
 
 }
