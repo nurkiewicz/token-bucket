cdda931f8347c14a1d5ac51e3e07c77fbff3c757
Rob Worsnop <rworsnop@gmail.com>
2013-10-30 14:38:55 -0400

    Switched to Guava's RateLimiter

--- src/main/java/com/blogspot/nurkiewicz/download/DownloadServletHandler.java
+++ src/main/java/com/blogspot/nurkiewicz/download/DownloadServletHandler.java
@@ -1,6 +1,6 @@
 package com.blogspot.nurkiewicz.download;
 
-import com.blogspot.nurkiewicz.download.tokenbucket.TokenBucket;
+import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
@@ -33,100 +33,94 @@ import static org.apache.commons.lang.time.DateUtils.MILLIS_PER_HOUR;
 @ManagedResource
 public class DownloadServletHandler implements HttpRequestHandler {
 
-	private static final Logger log = LoggerFactory.getLogger(DownloadServletHandler.class);
 
-	@Resource
-	private TokenBucket tokenBucket;
 
-	private AtomicLong requestNo = new AtomicLong();
+    private static final Logger log = LoggerFactory.getLogger(DownloadServletHandler.class);
+    private static final int CHUNK_SIZE = 1024*10;
+    private static final double RATE_PER_REQUEST = 1024;  // in kilobytes/s
 
-	@Resource
-	private ThreadPoolTaskExecutor downloadWorkersPool;
+       @Resource
+    private ThreadPoolTaskExecutor downloadWorkersPool;
 
-	@Override
-	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		long curRequestNo = requestNo.incrementAndGet();
-		request.setAttribute(TokenBucket.REQUEST_NO, curRequestNo);
-		log.info("Serving: {} ({})", request.getRequestURI(), curRequestNo);
-//		final File file = new File(req.getRequestURI());
-		final File file = new File("/home/dev/tmp/ehcache-1.6.2.jar");
-		final BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
-		response.setContentLength((int) file.length());
-		final AsyncContext asyncContext = request.startAsync(request, response);
-		asyncContext.setTimeout(MILLIS_PER_HOUR);
-		final DownloadChunkTask task = new DownloadChunkTask(asyncContext, input, curRequestNo);
-		asyncContext.addListener(task);
-		downloadWorkersPool.submit(task);
-	}
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	private class DownloadChunkTask implements Callable<Void>, AsyncListener {
+        final File file = new File("\\\\20ex15308\\share\\fakeimage.jpg");
+        final BufferedInputStream input = new BufferedInputStream(new FileInputStream(file));
+        response.setContentLength((int) file.length());
+        final AsyncContext asyncContext = request.startAsync(request, response);
+        asyncContext.setTimeout(MILLIS_PER_HOUR);
+        final DownloadChunkTask task = new DownloadChunkTask(asyncContext, input);
+        asyncContext.addListener(task);
+        downloadWorkersPool.submit(task);
+    }
 
-		private final BufferedInputStream fileInputStream;
-		private final byte[] buffer = new byte[TokenBucket.TOKEN_PERMIT_SIZE];
-		private final AsyncContext ctx;
-		private final long requestNo;
-		int chunkNo;
+    private class DownloadChunkTask implements Callable<Void>, AsyncListener {
 
-		public DownloadChunkTask(AsyncContext ctx, BufferedInputStream fileInputStream, long requestNo) throws IOException {
-			this.ctx = ctx;
-			this.requestNo = requestNo;
-			this.fileInputStream = fileInputStream;
-		}
+        private final BufferedInputStream fileInputStream;
+        private final byte[] buffer = new byte[CHUNK_SIZE];
+        private final AsyncContext ctx;
+        private final RateLimiter rateLimiter;
+        int chunkNo;
 
-		@Override
-		public Void call() throws Exception {
-			try {
-				if (tokenBucket.tryTake(ctx.getRequest())) {
-					sendChunkWorthOneToken();
-				} else
-					downloadWorkersPool.submit(this);
-			} catch (Exception e) {
-				log.error("Error while sending data chunk, aborting ("  + requestNo + ")", e);
-				ctx.complete();
-			}
-			return null;
-		}
+        public DownloadChunkTask(AsyncContext ctx, BufferedInputStream fileInputStream) throws IOException {
+            this.ctx = ctx;
+            this.fileInputStream = fileInputStream;
+            this.rateLimiter = RateLimiter.create(RATE_PER_REQUEST);
+        }
 
-		private void sendChunkWorthOneToken() throws IOException {
-			log.trace("Sending chunk {} of request ({})", chunkNo++, requestNo);
-			final int bytesCount = fileInputStream.read(buffer);
+        @Override
+        public Void call() throws Exception {
+            try {
+                if (rateLimiter.tryAcquire(CHUNK_SIZE / 1024)) {
+                    sendChunk();
+                } else
+                    downloadWorkersPool.submit(this);
+            } catch (Exception e) {
+                log.error("Error while sending data chunk, aborting", e);
+                ctx.complete();
+            }
+            return null;
+        }
+
+        private void sendChunk() throws IOException {
+            log.trace("Sending chunk {} ", chunkNo++);
+            final int bytesCount = fileInputStream.read(buffer);
             if (bytesCount > 0){
                 ctx.getResponse().getOutputStream().write(buffer, 0, bytesCount);
             }
-			if (bytesCount < buffer.length) {
-				ctx.complete();
-			} else {
-				downloadWorkersPool.submit(this);
-			}
-		}
+            if (bytesCount < buffer.length) {
+                ctx.complete();
+            } else {
+                downloadWorkersPool.submit(this);
+            }
+        }
 
-		@Override
-		public void onComplete(AsyncEvent asyncEvent) throws IOException {
-			fileInputStream.close();
-			tokenBucket.completed(ctx.getRequest());
-			log.debug("Done: ({})", requestNo);
-		}
+        @Override
+        public void onComplete(AsyncEvent asyncEvent) throws IOException {
+            fileInputStream.close();
+        }
 
-		@Override
-		public void onTimeout(AsyncEvent asyncEvent) throws IOException {
-			log.warn("Asynchronous request timeout ({})", requestNo);
-			onComplete(asyncEvent);
-		}
+        @Override
+        public void onTimeout(AsyncEvent asyncEvent) throws IOException {
+            log.warn("Asynchronous request timeout");
+            onComplete(asyncEvent);
+        }
 
-		@Override
-		public void onError(AsyncEvent asyncEvent) throws IOException {
-			log.warn("Asynchronous request error (" + requestNo + ")", asyncEvent.getThrowable());
-			onComplete(asyncEvent);
-		}
+        @Override
+        public void onError(AsyncEvent asyncEvent) throws IOException {
+            log.warn("Asynchronous request error", asyncEvent.getThrowable());
+            onComplete(asyncEvent);
+        }
 
-		@Override
-		public void onStartAsync(AsyncEvent asyncEvent) throws IOException {
-		}
-	}
+        @Override
+        public void onStartAsync(AsyncEvent asyncEvent) throws IOException {
+        }
+    }
 
-	@ManagedAttribute
-	public int getAwaitingChunks() {
-		return downloadWorkersPool.getThreadPoolExecutor().getQueue().size();
-	}
+    @ManagedAttribute
+    public int getAwaitingChunks() {
+        return downloadWorkersPool.getThreadPoolExecutor().getQueue().size();
+    }
 
 }
